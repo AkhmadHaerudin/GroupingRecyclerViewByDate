32c8e09daaba6051b59df81d63672b68f5c03f6a
Akhmad Haerudin <akhmad.haerudin@gmail.com>
2019-10-25 14:35:22 +0700

    Initial commit

---
+++ app/src/main/java/id/triliun/myapplication/ListItem.java
@@ -0,0 +1,9 @@
+package id.triliun.myapplication;
+
+public abstract class ListItem {
+
+    public static final int TYPE_DATE = 0;
+    public static final int TYPE_GENERAL = 1;
+
+    abstract public int getType();
+}
